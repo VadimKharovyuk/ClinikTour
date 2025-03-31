079465b385389376d5c63dc419b198d9415ef1e8
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-03-31 21:57:12 +0300

    update blog

--- src/main/java/com/example/cliniktour/ClinikTourApplication.java
+++ src/main/java/com/example/cliniktour/ClinikTourApplication.java
@@ -23,6 +23,16 @@ public class ClinikTourApplication {
     //Публикации и достижения врача...
 
 
+    //  <!-- Категории блога (опционально) -->
+    //<!--  <div class="blog-categories">-->
+    //<!--    <a href="#" class="category-pill active">Все статьи</a>-->
+    //<!--    <a href="#" class="category-pill">Лечение за рубежом</a>-->
+    //<!--    <a href="#" class="category-pill">Советы пациентам</a>-->
+    //<!--    <a href="#" class="category-pill">Клиники</a>-->
+    //<!--    <a href="#" class="category-pill">Интересные факты</a>-->
+    //<!--  </div>-->
+
+
 
 
 
